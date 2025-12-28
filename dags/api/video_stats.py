a6c9da86ff236a7413ffbc519e1f059ee16be62f
Matthew Beneventano <mattbeneven@gmail.com>
2025-12-27 19:59:58 -0800

    lots of changes

--- dags/api/video_stats.py
+++ dags/api/video_stats.py
@@ -2,18 +2,20 @@ import requests
 import json
 from datetime import date
 
-import os
-from dotenv import load_dotenv
+# import os
+# from dotenv import load_dotenv
+# load_dotenv(dotenv_path="./.env")
 
-load_dotenv(dotenv_path="./.env")
+from airflow.decorators import task
+from airflow.models import Variable
 
-API_KEY = os.getenv("API_KEY")
+API_KEY =  Variable.get("API_KEY")
 
-CHANNEL_HANDLE = "MrBeast"
+CHANNEL_HANDLE = Variable.get("CHANNEL_HANDLE")
 
 maxResults = 50
 
-
+@task
 def get_playlist_id():
 
     try: 
@@ -36,6 +38,7 @@ def get_playlist_id():
         raise e
 
 
+@task
 def get_video_ids(playlistId):
     video_ids = []
     pageToken = None
@@ -69,7 +72,7 @@ def get_video_ids(playlistId):
         raise e
 
 
-
+@task
 def extract_video_data(video_ids):
     extracted_data = []
 
@@ -112,12 +115,13 @@ def extract_video_data(video_ids):
     except requests.exceptions.RequestException as e:
         raise e
 
-
+@task
 def save_to_json(extracted_data):
     file_path = f"./data/YT_data_{date.today()}.json"
 
     with open(file_path, "w", encoding = "utf-8") as json_outfile:
         json.dump(extracted_data, json_outfile, indent=4, ensure_ascii=False)
+
 
 if __name__ == "__main__":
     playlistId = get_playlist_id()
