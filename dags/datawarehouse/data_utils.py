62db0d170bfa15a29bda07e68f49f5f18dbd92d5
Matthew Beneventano <mattbeneven@gmail.com>
2025-12-29 09:35:49 -0800

    conn and cur setup

--- dags/datawarehouse/data_utils.py
+++ dags/datawarehouse/data_utils.py
@@ -5,8 +5,8 @@ table = "yt_api"
 
 def get_conn_cursor():
     hook = PostgresHook(postgres_conn_id="postgres_db_yt_elt", database="elt_db")
-    conn = hook.get_conn()
-    cur = conn.cursor(cursfor_factory=RealDictCursor)
+    conn = hook.get_conn() # connection is used to establish connection to postgres instance
+    cur = conn.cursor(cursfor_factory=RealDictCursor) # cursor is an object that allows you to execute SQL commands for the relevant connection
     return conn, cur
 
 def close_conn_cursor(conn, cur):
@@ -21,3 +21,49 @@ def create_schema(schema):
     cur.execute(schema_sql)
 
     conn.commit()
+
+    close_conn_cursor(conn, cur)
+
+def create_table(schema):
+    conn, cur = get_conn_cursor()
+
+    if schema == 'staging':
+        table_sql = f"""
+                CREATE TABLE IF NOT EXISTS {schema}.{table} (
+                    "Video_ID" VARCHAR(11) PRIMARY KEY NOT NULL,
+                    "Video_Title" TEXT NOT NULL,
+                    "Upload_Date" TIMESTAMP NOT NULL,
+                    "Duration" VARCHAR(20) NOT NULL,
+                    "Video_Views" INT,
+                    "Likes_Count" INT,
+                    "Comments_Count" INT   
+                );
+            """
+    else:
+        table_sql = f"""
+                  CREATE TABLE IF NOT EXISTS {schema}.{table} (
+                      "Video_ID" VARCHAR(11) PRIMARY KEY NOT NULL,
+                      "Video_Title" TEXT NOT NULL,
+                      "Upload_Date" TIMESTAMP NOT NULL,
+                      "Duration" TIME NOT NULL,
+                      "Video_Type" VARCHAR(10) NOT NULL,
+                      "Video_Views" INT,
+                      "Likes_Count" INT,
+                      "Comments_Count" INT    
+                  ); 
+              """
+
+    cur.execute(table_sql)
+
+    conn.commit()
+
+    close_conn_cursor(conn, cur)
+
+    def get_video_ids(cur, schema):
+
+        cur.execute(f"""SELECT "Video_ID" FROM {schema}.{table};""")
+        ids = cur.fetchall()
+
+        video_ids = [row["Video_ID"] for row in ids]
+
+        return video_ids
